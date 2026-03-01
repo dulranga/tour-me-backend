55383103095aa662b176ed717c1bddfd966e3219
dulranga <dulranga.webdesign@gmail.com>
2026-03-01 12:35:42 +0530

    first test route

--- src/main/java/com/tourme/tourme/TourmeApplication.java
+++ src/main/java/com/tourme/tourme/TourmeApplication.java
@@ -2,12 +2,20 @@ package com.tourme.tourme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TourmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourmeApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Tourme!";
 	}
 
 }
