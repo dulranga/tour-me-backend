d9ef8967be4471dd7bfecc3adb75dd2d9bb3d0f5
dulranga <dulranga.webdesign@gmail.com>
2026-03-01 12:26:09 +0530

    initial commit from spring config

---
+++ src/main/java/com/tourme/tourme/TourmeApplication.java
@@ -0,0 +1,13 @@
+package com.tourme.tourme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourme/tourme/TourmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourme.tourme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
