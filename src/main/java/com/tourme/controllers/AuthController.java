67a3967631ffbf6bd0dc8602334b649032aa8dc1
Desandu123 <desanduwanniarachchi@gmail.com>
2026-03-24 02:39:42 +0530

    completed authentication controller

--- src/main/java/com/tourme/controllers/AuthController.java
+++ src/main/java/com/tourme/controllers/AuthController.java
@@ -1,26 +1,50 @@
 package com.tourme.controllers;
 
+import com.tourme.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
+    /*
+    check the credentials of the user and if they are valid, generate a JWT token and return it in the response. 
+     * @param loginRequest - A map containing the user's email and password. this is json file which we convert to map and then pass it to service.
+     * call the service to check the credentials of the user and if they are valid, generate a JWT token and return it in the response.
+    */
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Object loginRequest) {
-        // to be implemented: validate credentials, generate JWT token, etc.
-        return null;
+    public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
+        String email = loginRequest.get("email");
+        String password = loginRequest.get("password");
+        return authService.login(email, password);
     }
 
+    /*
+    authenticate the user by their JWT token and invalidate the token to log the user out. 
+     * @param logoutRequest - A map containing the user's JWT token. this is json file which we convert to map and then pass it to service.
+     * call the service to authenticate the user by their JWT token and invalidate the token to log the user out.
+    */
     @PostMapping("/logout")
-    public ResponseEntity<?> logout() {
-        // to be implemented: validate credentials, generate JWT token, etc.
-        return null;
+    public ResponseEntity<?> logout(@RequestBody Map<String, String> logoutRequest) {
+        String token = logoutRequest.get("token");
+        return authService.logout(token);
     }
 
+    /*
+    refresh the user's JWT token by validating the provided refresh token and generating a new access token if the refresh token is valid. 
+     * @param refreshRequest - A map containing the user's refresh token. this is json file which we convert to map and then pass it to service.
+     * call the service to refresh the user's JWT token by validating the provided refresh token and generating a new access token if the refresh token is valid.
+    */
     @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken(@RequestBody String token) {
-        return null;
+    public ResponseEntity<?> refreshToken(@RequestBody Map<String, String> refreshRequest) {
+        String refreshToken = refreshRequest.get("refreshToken");
+        return authService.refreshToken(refreshToken);
     }
 }
