ccb91b3a1b9bd6f2caa64f422e63233acaf5e89c
dulranga <dulranga.webdesign@gmail.com>
2026-03-26 17:33:40 +0530

    added methods to register users and get them all

--- src/main/java/com/tourme/services/UserService.java
+++ src/main/java/com/tourme/services/UserService.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.tourme.models.Administrator;
+import com.tourme.models.Driver;
 import com.tourme.models.Tourist;
 import com.tourme.models.User;
 import com.tourme.repositories.UserRepository;
+import com.tourme.exceptions.UserNotFoundException;
 
 @Service
 public class UserService {
@@ -32,5 +35,54 @@ public class UserService {
         tourist.setPasswordHash(passwordEncoder.encode(password));
 
         return userRepository.save(tourist);
+    }
+
+    public Driver registerDriver(Driver driver) {
+        String password = driver.getPasswordHash();
+        driver.setPasswordHash(passwordEncoder.encode(password));
+
+        return userRepository.save(driver);
+    }
+
+    public Administrator registerAdmin(Administrator admin) {
+        String password = admin.getPasswordHash();
+        admin.setPasswordHash(passwordEncoder.encode(password));
+
+        return userRepository.save(admin);
+    }
+
+    public User updateProfile(int id, User profileData) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException());
+
+        user.setName(profileData.getName());
+        user.setEmail(profileData.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    public Driver updateVehicleDetails(int id, String vehicleDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException());
+
+        if (!(user instanceof Driver)) {
+            throw new IllegalArgumentException("User is not a driver");
+        }
+
+        Driver driver = (Driver) user;
+        driver.setVehicleDetails(vehicleDetails);
+        return userRepository.save(driver);
+    }
+
+    public List<User> getAllTourists() {
+        return userRepository.findAll().stream()
+                .filter(user -> user instanceof Tourist)
+                .toList();
+    }
+
+    public List<User> getAllDrivers() {
+        return userRepository.findAll().stream()
+                .filter(user -> user instanceof Driver)
+                .toList();
     }
 }
