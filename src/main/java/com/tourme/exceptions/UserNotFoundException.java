d7429dbba882f7578fe4a08b6b4034786c997998
Nadish Sathsara <nadishsathsara2003@gmail.com>
2026-04-29 21:31:04 +0530

    Update UserNotFoundException.java

--- src/main/java/com/tourme/exceptions/UserNotFoundException.java
+++ src/main/java/com/tourme/exceptions/UserNotFoundException.java
@@ -1,10 +1,13 @@
 package com.tourme.exceptions;
 
+// Custom exception class for user not found errors
 public class UserNotFoundException extends RuntimeException {
+
+    // Default constructor with predefined message
     public UserNotFoundException() {
         super("User not found");
     }
-
+    // Constructor that accepts a custom error message
     public UserNotFoundException(String message) {
         super(message);
     }
