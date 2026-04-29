48f3ab04ad5acd6e4740a81be973d4724a4730c9
Nadish Sathsara <nadishsathsara2003@gmail.com>
2026-04-29 21:28:35 +0530

    Add custom ForbiddenException for access errors

--- src/main/java/com/tourme/exceptions/ForbiddenException.java
+++ src/main/java/com/tourme/exceptions/ForbiddenException.java
@@ -1,10 +1,14 @@
 package com.tourme.exceptions;
 
+// Custom exception class for forbidden access errors
 public class ForbiddenException extends RuntimeException {
+
+    // Default constructor with predefined message
     public ForbiddenException() {
         super("Access forbidden");
     }
 
+     // Constructor that accepts a custom error message
     public ForbiddenException(String message) {
         super(message);
     }
